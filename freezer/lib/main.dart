a1decad747ecb8a2c90670b247bec69e42405197
Júlio Santos <julio.santos@onefootball.com>
2024-12-03 11:59:54 +0100

    remove modular dependencie

--- freezer/lib/main.dart
+++ freezer/lib/main.dart
@@ -1,67 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 Future<void> main() async {
-  runApp(driver);
+  runApp(MyApp());
 }
 
 get fakeClubId => "1094";
 
 get fakeNationalId => "1";
 
-final ModularApp driver = ModularApp(
-  module: WrapperModule(),
-  child: const SafeArea(child: MyApp()),
-);
-
-class WrapperModule extends Module {
-  @override
-  List<ModularRoute> get routes => [
-        ChildRoute(
-          '/home',
-          child: (context, args) => Scaffold(
-              body: SingleChildScrollView(
-            child: Column(
-              children: List<Widget>.generate(
-                1000,
-                (index) => Text('this is home dummy text index $index'),
-              ),
-            ),
-          )),
-        ),
-        ChildRoute(
-          '/profile',
-          child: (context, args) => Scaffold(
-              body: SingleChildScrollView(
-            child: Column(children: [
-              SizedBox(
-                width: 100,
-                height: 50,
-                child: UiKitView(
-                  viewType: "freezer",
-                  creationParamsCodec: StandardMessageCodec(),
-                ),
-              ),
-              ...List<Widget>.generate(
-                1000,
-                (index) => Text('this is profile dummy text index $index'),
-              ),
-            ]),
-          )),
-        )
-      ];
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'Home',
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
+      initialRoute: '/home',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/home': (context) => Scaffold(
+                body: SingleChildScrollView(
+              child: Column(
+                children: List<Widget>.generate(
+                  1000,
+                  (index) => Text('this is home dummy text index $index'),
+                ),
+              ),
+            )),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/profile': (context) => Scaffold(
+                body: SingleChildScrollView(
+              child: Column(children: [
+                const SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: UiKitView(
+                    viewType: "freezer",
+                    creationParamsCodec: StandardMessageCodec(),
+                  ),
+                ),
+                ...List<Widget>.generate(
+                  1000,
+                  (index) => Text('this is profile dummy text index $index'),
+                ),
+              ]),
+            )),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
